573ebbbc131ac473c39439cb5fb9157a9e351286
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-09-23 13:21:22 +0000

    wip

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_POST['nom'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 $message = $_POST['message'];
-$to = "Contact@stayinmedia.net";
+$to = "Contact@chargether.com";
 $subject = "email depuis le site web";
 $txt ="Nom = ". $name . "\r\n  Email = " . $email . "\r\n Téléphone = ". $tel ."\r\n Message =" . $message;
 $headers = "From: no-reply@chargether.com" . "\r\n" .
